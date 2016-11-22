33a9888bfa75df9efdee0d77f87affb4aa909852
Alex <aseceans@gmail.com>
2016-11-22 01:08:51 -0500

    added nav drawer like td

--- TapSecure/app/src/main/java/com/example/alexandryan/tapsecure/MainTDAppActivity.java
+++ TapSecure/app/src/main/java/com/example/alexandryan/tapsecure/MainTDAppActivity.java
@@ -1,25 +1,54 @@
 package com.example.alexandryan.tapsecure;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
 import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+import static com.example.alexandryan.tapsecure.R.id.fragment_container;
+
 public class MainTDAppActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, TDHomeFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tdapp);
+
+        //fragment stuff
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+           // setTitle("Live Feedback");
+            // Create a new Fragment to be placed in the activity layout
+            TDHomeFragment liveFeedbackFrag = new TDHomeFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            liveFeedbackFrag.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            //getSupportFragmentManager().beginTransaction().add(fragment_container, liveFeedbackFrag).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, liveFeedbackFrag).commit();
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -70,7 +99,7 @@ public class MainTDAppActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+/*
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
@@ -84,9 +113,15 @@ public class MainTDAppActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
-
+*/
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
