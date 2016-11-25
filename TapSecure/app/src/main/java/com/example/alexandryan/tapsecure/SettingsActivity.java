fd9c4801f17dbc18398c10a6548ec40ed71db0f0
Alex <aseceans@gmail.com>
2016-11-25 15:46:49 -0500

    added permissions and active switch

--- TapSecure/app/src/main/java/com/example/alexandryan/tapsecure/SettingsActivity.java
+++ TapSecure/app/src/main/java/com/example/alexandryan/tapsecure/SettingsActivity.java
@@ -18,9 +18,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
-
 import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
