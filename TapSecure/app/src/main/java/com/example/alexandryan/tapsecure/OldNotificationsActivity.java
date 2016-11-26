a2f7a2b1b3554931156c606994c5a831132af2c5
Alex <aseceans@gmail.com>
2016-11-26 18:44:24 -0500

    settings spinner still broken. fix later

--- TapSecure/app/src/main/java/com/example/alexandryan/tapsecure/OldNotificationsActivity.java
+++ TapSecure/app/src/main/java/com/example/alexandryan/tapsecure/OldNotificationsActivity.java
@@ -12,24 +12,10 @@ public class OldNotificationsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications_old);
 
-        createSpinner();
+
         setTitle("TD TapSecure Notifications");
         //NOTE: back button page routing is declared in manifest as the parent
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    public void createSpinner(){
-        // Creating adapter for spinner
-        accountSpinner = (Spinner) findViewById(R.id.accountSpinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item, getResources().getStringArray(R.array.accounts)); //
-
-        // attaching data adapter to spinner
-        accountSpinner.setAdapter(adapter);
-        // Drop down layout style - list view with radio button
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        setTitle("TD TapSecure Settings");
-        //NOTE: back button page routing is declared in manifest as the parent
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 }
--- TapSecure/app/src/main/java/com/example/alexandryan/tapsecure/SettingsActivity.java
+++ TapSecure/app/src/main/java/com/example/alexandryan/tapsecure/SettingsActivity.java
@@ -1,19 +1,15 @@
 package com.example.alexandryan.tapsecure;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -22,11 +18,11 @@ import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
-
     Spinner accountSpinner;
     EditText tapDollarDisplay;
     Switch tapSecureEnabledSwitch;
@@ -39,6 +35,7 @@ public class SettingsActivity extends AppCompatActivity {
         tapSecureSettingsHolder = (ScrollView) findViewById(R.id.tapSecureSettingsHolder);
         tapSecureSettingsHolder.setVisibility(View.INVISIBLE);
         tapSecureEnabledSwitch = (Switch) findViewById(R.id.tapSecureEnabledSwitch);
+
         tapSecureEnabledSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
@@ -51,40 +48,39 @@ public class SettingsActivity extends AppCompatActivity {
 
         setTitle("TD TapSecure Settings");
         tapDollarDisplay = (EditText) findViewById(R.id.amountEntered);
-        setCursorVisibility();
+        tapDollarDisplay.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                System.out.println("text changed to " + tapDollarDisplay.getText());
+                Float myF = Float.parseFloat(tapDollarDisplay.getText().toString());
+                BankService.DebitCard.setTapLimit(myF);
+            }
+        });
+
         createSpinner();
+        updateTapDollarDisplayFromSharedPrefs();
+
     }
 
-    public void setCursorVisibility(){
-        //this line prevents keyboard from opening when activity launches
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-
-        tapDollarDisplay.setCursorVisible(false);
-
-        tapDollarDisplay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tapDollarDisplay.setCursorVisible(true);
-            }
-        });
-
-        tapDollarDisplay.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                tapDollarDisplay.setCursorVisible(false);
-                if (event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
-                    InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                    in.hideSoftInputFromWindow(tapDollarDisplay.getApplicationWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
-                }
-                return true;
-            }
-        });
+    public void updateTapDollarDisplayFromSharedPrefs(){
+        String cardText = accountSpinner.getSelectedItem().toString();
+        if(cardText.equals("TD VISA - 2355336522565984")){
+            Float f = BankService.getSharedPrefs().getFloat("VTapLimit",100f);
+            tapDollarDisplay.setText(f.toString());
+        } else if (cardText.equals("EVERY DAY CHEQUING - 3365864")){
+            Float f = BankService.getSharedPrefs().getFloat("VTapLimit",100f);
+            tapDollarDisplay.setText(f.toString());
+        }
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        tapDollarDisplay.setFocusable(false);
+    public void makeToast(){
+        Toast.makeText(this, "done pressed", Toast.LENGTH_SHORT).show();
     }
 
     public void createSpinner(){
@@ -98,29 +94,52 @@ public class SettingsActivity extends AppCompatActivity {
 
         // Creating adapter for spinner
         accountSpinner = (Spinner) findViewById(R.id.accountSpinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item, accounts); //
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item, accounts);
 
         // attaching data adapter to spinner
         accountSpinner.setAdapter(adapter);
         // Drop down layout style - list view with radio button
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        accountSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                updateTapDollarDisplayFromSharedPrefs();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         //NOTE: back button page routing is declared in manifest as the parent
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     public void onDollarAmtClick(View view) {
-
         switch (view.getId()){
             case R.id.button50:
                 tapDollarDisplay.setText("50");
+                saveTapLimitInSharedPrefs(50f);
                 break;
             case R.id.button100:
                 tapDollarDisplay.setText("100");
+                saveTapLimitInSharedPrefs(100f);
                 break;
             case R.id.button250:
                 tapDollarDisplay.setText("250");
+                saveTapLimitInSharedPrefs(250f);
                 break;
+        }
+    }
+
+    public void saveTapLimitInSharedPrefs(float amt){
+        if(BankService.getSharedPrefs().getBoolean("VInteracFlashEnabled", false)){ //if visa
+            BankService.VisaCard.setTapLimit(amt);
+        }
+        if (BankService.getSharedPrefs().getBoolean("DInteracFlashEnabled", false)){ //if debit
+            BankService.DebitCard.setTapLimit(amt);
         }
     }
 }
